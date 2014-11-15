c270ad93f5524a4ca67e3eb2baf91c26b5feb97a
Brice Dobry <brice@obycode.com>
2014-11-15 15:02:05 -0500

    Detail disclosure; get spots for specific location (with radius)

---
+++ Bikestack/BSSpotAnnotation.swift
@@ -0,0 +1,9 @@
+//
+//  BSSpotAnnotation.swift
+//  Bikestack
+//
+//  Created by Brice Dobry on 11/15/14.
+//  Copyright (c) 2014 obycode. All rights reserved.
+//
+
+import Foundation
--- Bikestack/ViewController.swift
+++ Bikestack/ViewController.swift
@@ -14,11 +14,14 @@ let apiGetSpots = "/api/spots" //GET
 let apiCreateSpot = "/api/spots"  // POST: lock_up -> (name, lat, lon, description, capacity)
 let apiFindSpots = "/api/spots/find" // POST: lat, lon, radius (miles, defaults to .1)
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var spotDetailView: UIView!
     
     var locationManager: CLLocationManager!
+    var currentSpots: Array<MKAnnotation>!
+    var inited: Bool!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,27 +35,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.startUpdatingLocation()
         }
         
-        var request = HTTPTask()
-        request.baseURL = apiBaseUrl
-
-        //The expected response will be JSON and be converted to an object return by NSJSONSerialization instead of a NSData.
-        request.responseSerializer = JSONResponseSerializer()
-        println("getting spots...")
-        request.GET(apiGetSpots, parameters: nil, success: {(response: HTTPResponse) in
-            if let spotList = response.responseObject as? Array< Dictionary<String,AnyObject> > {
-                self.addPointsFromList(spotList)
-            } else {
-                println("response was not a dict: \(response)")
-            }
-            }, failure: {(error: NSError, response: HTTPResponse?) in
-                println("print the error: \(error)")
-                println("print the response: \(response)")
-            })
-        
-        let span = MKCoordinateSpanMake(0.01, 0.01)
-        let region = MKCoordinateRegion(center: locationManager.location.coordinate, span: span)
-        mapView.setRegion(region, animated: true)
-        mapView.showsUserLocation = true
+        inited = false
     }
     
     @IBAction func submitPressed(sender: AnyObject) {
@@ -60,6 +43,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         var request = HTTPTask()
         request.baseURL = apiBaseUrl
+        
+        println("params are \(params)")
         
         request.POST(apiCreateSpot, parameters: params, success: {(response: HTTPResponse) in
             println("Got data from \(apiBaseUrl + apiCreateSpot)")
@@ -76,15 +61,72 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
-//        let location = locations.last as CLLocation
-//        
-//        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-//        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-//        
-//        mapView.setRegion(region, animated: true)
+        // Center to current location on first launch
+        if (!inited) {
+            inited = true
+            
+            let location = locations.last as CLLocation
+            
+            let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            
+            mapView.setRegion(region, animated: false)
+        }
+    }
+
+    func mapView(mapView: MKMapView!, regionDidChangeAnimated animated: Bool) {
+        // if we haven't already zoomed to the current location, don't get spots yet
+        if (!inited) {
+            return
+        }
+        
+        var request = HTTPTask()
+        request.baseURL = apiBaseUrl
+        
+        //The expected response will be JSON and be converted to an object return by NSJSONSerialization instead of a NSData.
+        request.responseSerializer = JSONResponseSerializer()
+        println("getting spots...")
+        let dict: Dictionary<String, AnyObject> = ["lat" : locationManager.location.coordinate.latitude, "lon" : locationManager.location.coordinate.longitude, "rad" : (mapView.region.span.latitudeDelta)*69]
+        println("latitude Delta is \(mapView.region.span.latitudeDelta)")
+        let params: Dictionary<String,AnyObject> = ["lock_up": dict]
+        println("params are \(params)")
+        request.POST(apiFindSpots, parameters: params, success: {(response: HTTPResponse) in
+            if let spotList = response.responseObject as? Array< Dictionary<String,AnyObject> > {
+                self.addPointsFromList(spotList)
+            } else {
+                println("response was not a dict: \(response)")
+            }
+            }, failure: {(error: NSError, response: HTTPResponse?) in
+                println("print the error: \(error)")
+                println("print the response: \(response)")
+        })
     }
     
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        if annotation is MKUserLocation {
+            //return nil so map view draws "blue dot" for standard user location
+            return nil
+        }
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinColor = .Green
+            pinView?.rightCalloutAccessoryView = UIButton.buttonWithType(UIButtonType.DetailDisclosure) as UIView
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+
     func addPointsFromList(list: Array< Dictionary<String,AnyObject> >) {
+        mapView.removeAnnotations(currentSpots)
+        
         println("add points from \(list)")
         for item in list {
             let spot = BSSpot(jsonDict: item)
